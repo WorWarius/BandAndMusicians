9ff677dc87ba4f36d328406fe90767424352c8c6
WorWarius <linus.s.edlund@gmail.com>
2023-10-13 14:10:53 +0200

    Added variables and ideas
    I added some of the variables i think was needed and some ideas I have of how to implement things in a easy way

---
+++ classes.js
@@ -0,0 +1,20 @@
+export class musician {
+  constructor(info, name, birthDay, currentBands, previousBands, instruments) {
+    this.information = info;
+    this.theName = name;
+    this.theBirthDay = birthDay;
+    this.theCurrentBands = currentBands
+    this.thePreviousBands = previousBands
+    this.theInstruments = instruments
+  }
+
+}
+export class band {
+  constructor() {
+    this.bandInformation = info;
+    this.foundation = birthDay;
+    this.disbandDate = currentBands
+    this.membersInfo()
+    this.previousMemberInfo()
+  }
+}
---
+++ main.js
@@ -0,0 +1,30 @@
+let info;
+let namn;
+let birthDay;
+let currentBands = []
+let previousBands = []
+let instrument = []
+
+let bandInfo;
+let foundationDate;
+let disbandment;
+let theMembersInfo = []
+let previousMemberInfo = []
+
+
+
+
+
+
+
+function Menu() {
+  console.log("1. Lägg till musiker")
+  console.log("2. Lägg till band")
+  console.log("3. Ta bort Band")
+  console.log("4. Ta bort Musiker")
+  console.log("5. Lägg till en musiker i ett band") //Ha att en lista kommer upp på alla musiker i nummer där först man väljer personen och sedan för välja bandet i en nummer lista för att lägga till den
+  console.log("6. Ta bort musiker från ett band") //Samma sak som innan men att man väljer en musiker och sedan kommer endast bandet upp på det band som musikern du valt är med i
+  console.log("7. Information om musiker") //En lista på alla musiker och den man väljer får man information om¨
+  console.log("8. Information om band") //percis som den över men istället har den info om band
+  console.log("9. Avsluta")
+}
